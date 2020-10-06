489a123c0b285dda62c933e171d2b42c3d6fc3de
Deborah Evelyn Carlota <evelyndec92@gmail.com>
2020-10-06 19:40:15 -0300

    Update

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -7,11 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "postagem")
@@ -31,9 +34,12 @@ public class Postagem {
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date data = new Date(System.currentTimeMillis());
 	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 	
-	// Getters and Setters	
-	
+		
+
 	public Long getId() {
 		return id;
 	}
@@ -64,5 +70,13 @@ public class Postagem {
 
 	public void setData(Date data) {
 		this.data = data;
+	}
+	
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 }
