a8ed0919ad29c9ea6d36a16941cc692853bb255d
Deborah Evelyn Carlota <evelyndec92@gmail.com>
2020-10-06 10:47:17 -0300

    Atualização do CRUD

---
+++ MinhaEscola/MinhaEscola/src/main/java/com/minha/escola/repository/AlunesRepository.java
@@ -0,0 +1,13 @@
+package com.minha.escola.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.minha.escola.model.Alunes;
+
+@Repository
+public interface AlunesRepository extends JpaRepository<Alunes, Long>{
+	
+	
+}
