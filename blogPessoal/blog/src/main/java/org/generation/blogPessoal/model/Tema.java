31fb9068c8ba1479f42e931168bc97b5454ff67b
Deborah Evelyn Carlota <evelyndec92@gmail.com>
2020-10-07 10:21:35 -0300

    Atualização do blog pessoal, para estudarmos o relacionamento entre as tabelas

--- blogPessoal/blog/src/main/java/org/generation/blogPessoal/model/Tema.java
+++ blogPessoal/blog/src/main/java/org/generation/blogPessoal/model/Tema.java
@@ -20,7 +20,7 @@ public class Tema {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	
 	@NotNull
 	private String descricao;
@@ -29,11 +29,11 @@ public class Tema {
 	@JsonIgnoreProperties("tema")
 	private List<Postagem> postagem;
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -52,7 +52,5 @@ public class Tema {
 	public void setPostagem(List<Postagem> postagem) {
 		this.postagem = postagem;
 	}
-	
-	
 
 }
